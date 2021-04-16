439f378c420847a68e77aa78cf602b3d9f81741a
Betselot-GA <betselotgetnet2227@gmail.com>
2021-04-16 18:02:47 +0300

    EJS name fix

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,27 @@
 const express = require("express");
-const adminRouter = require("../routes/admin")
+const adminRouter = require("../routes/admin");
 const router = express.Router();
-const {ensureAuthenticated} = require("../config/auth"); 
+const { ensureAuthenticated } = require("../config/auth");
 // const getVideo = require('../controllers/post-controller');
-const Post = require("../models/Post")
+const Post = require("../models/Post");
 
 //Welcome Page
-router.get("/",(req,res)=> res.render("Welcome"))
+router.get("/", (req, res) => res.render("welcome"));
 
 //Dashboard
-router.get("/index",ensureAuthenticated,(req,res)=>{
-Post.find({}, function(err, posts){
+router.get("/index", ensureAuthenticated, (req, res) => {
+  Post.find({}, function (err, posts) {
     res.render("home", {
-    name:req.user.name,
-    posts:posts
-
+      name: req.user.name,
+      posts: posts,
     });
     console.log(posts[0]);
-})
+  });
 });
 
-
-
-router.get("/admin",ensureAuthenticated,(req,res)=>{
-    res.render("admin")
-})
-router.post("/admin",ensureAuthenticated,adminRouter)
+router.get("/admin", ensureAuthenticated, (req, res) => {
+  res.render("admin");
+});
+router.post("/admin", ensureAuthenticated, adminRouter);
 
 module.exports = router;
